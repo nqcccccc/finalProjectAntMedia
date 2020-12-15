96a2f4c112ae1b7f2da6b20cadf4734d898f2423
nqcccccc <64541241+nqcccccc@users.noreply.github.com>
2020-12-15 23:38:03 +0700

    FAB"

--- app/src/main/java/xyz/nguyenquoccuong/mymeeting/Dashboard/DashboardActivity.java
+++ app/src/main/java/xyz/nguyenquoccuong/mymeeting/Dashboard/DashboardActivity.java
@@ -3,14 +3,58 @@ package xyz.nguyenquoccuong.mymeeting.Dashboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import xyz.nguyenquoccuong.mymeeting.R;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FloatingActionButton fab,fabNew,fabJoin;
+    private boolean isFABOpen = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        init();
     }
+
+    private void init() {
+        fab = findViewById(R.id.fab);
+        fabNew = findViewById(R.id.fabNew);
+        fabJoin = findViewById(R.id.fabJoin);
+
+        fabNew.setVisibility(View.INVISIBLE);
+        fabJoin.setVisibility(View.INVISIBLE);
+
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.fab:
+                fabNew.setVisibility(View.VISIBLE);
+                fabJoin.setVisibility(View.VISIBLE);
+                openFAB();
+                break;
+        }
+    }
+
+    private void openFAB() {
+        if(!isFABOpen){
+            isFABOpen=true;
+            fabNew.animate().translationY(-getResources().getDimension(R.dimen.standard_155));
+            fabJoin.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
+        } else{
+            isFABOpen=false;
+            fabNew.animate().translationY(0);
+            fabJoin.animate().translationY(0);
+        }
+    }
+
+
 }
