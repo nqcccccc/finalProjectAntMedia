1b6231bc65f682c994b0ffaab177361207573251
nqcccccc <64541241+nqcccccc@users.noreply.github.com>
2020-12-21 13:19:19 +0700

    Sign Out added

--- app/src/main/java/io/antmedia/android/Dashboard/DashboardAcitivity.java
+++ app/src/main/java/io/antmedia/android/Dashboard/DashboardAcitivity.java
@@ -12,7 +12,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -27,6 +30,7 @@ import java.util.ArrayList;
 import de.hdodenhof.circleimageview.CircleImageView;
 import io.antmedia.android.Broadcast.BroadcastActivity;
 import io.antmedia.android.Play.PlayActivity;
+import io.antmedia.android.SignIn.SignInActivity;
 import io.antmedia.android.User;
 import io.antmedia.android.broadcaster.R;
 
@@ -36,6 +40,7 @@ public class DashboardAcitivity extends AppCompatActivity implements View.OnClic
     private CircleImageView imgAva;
     private boolean isFABOpen = true;
     private String email,userID;
+    private User user;
     public static final String RTMP_BASE_URL = "rtmp://nguyenquoccuong.xyz/LiveApp/";
 
     @Override
@@ -60,7 +65,7 @@ public class DashboardAcitivity extends AppCompatActivity implements View.OnClic
         getUser.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                User user = dataSnapshot.getValue(User.class);
+                user = dataSnapshot.getValue(User.class);
                 Log.d("TAG", "onDataChange: "+user.getAvatar());
                 Picasso.with(DashboardAcitivity.this).load(user.getAvatar()).into(imgAva);
             }
@@ -85,6 +90,7 @@ public class DashboardAcitivity extends AppCompatActivity implements View.OnClic
         fabJoin.setOnClickListener(this);
         fabNew.setOnClickListener(this);
         fab.setOnClickListener(this);
+        imgAva.setOnClickListener(this);
     }
 
     @Override
@@ -102,22 +108,60 @@ public class DashboardAcitivity extends AppCompatActivity implements View.OnClic
                 break;
             case R.id.fabJoin:
                 openJoinD();
+                break;
+            case R.id.imgAva:
+                openInfoD();
+                break;
         }
     }
 
+    private void openInfoD() {
+        LayoutInflater inflaterI = getLayoutInflater();
+        View alertLayoutI = inflaterI.inflate(R.layout.info_dialog, null);
+
+        LinearLayout signout = alertLayoutI.findViewById(R.id.signout);
+        LinearLayout info = alertLayoutI.findViewById(R.id.info);
+        CircleImageView imgAvaD = alertLayoutI.findViewById(R.id.imgAvaD);
+        TextView tvName = alertLayoutI.findViewById(R.id.tvName);
+
+        Picasso.with(DashboardAcitivity.this).load(user.getAvatar()).into(imgAvaD);
+        tvName.setText(user.getFullName().trim());
+
+        AlertDialog.Builder alertI = new AlertDialog.Builder(this);
+        alertI.setView(alertLayoutI);
+
+        final AlertDialog dialogI = alertI.create();
+        dialogI.show();
+
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DashboardAcitivity.this, SignInActivity.class));
+                finish();
+            }
+        });
+
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DashboardAcitivity.this, "App info", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     private void openJoinD() {
-        LayoutInflater inflater = getLayoutInflater();
-        View alertLayout = inflater.inflate(R.layout.join_dialog, null);
-        final EditText txtRoomID = alertLayout.findViewById(R.id.txtRoomID);
-        Button btnSubmit = alertLayout.findViewById(R.id.btnSubmit);
-        final ProgressBar progressBar_submit = alertLayout.findViewById(R.id.progressBar_submit);
+        LayoutInflater inflaterJ = getLayoutInflater();
+        View alertLayoutJ = inflaterJ.inflate(R.layout.join_dialog, null);
+        final EditText txtRoomID = alertLayoutJ.findViewById(R.id.txtRoomID);
+        Button btnSubmit = alertLayoutJ.findViewById(R.id.btnSubmit);
+        final ProgressBar progressBar_submit = alertLayoutJ.findViewById(R.id.progressBar_submit);
 
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setTitle("Join Your Meet");
-        alert.setView(alertLayout);
+        AlertDialog.Builder alertJ = new AlertDialog.Builder(this);
+        alertJ.setTitle("Join Your Meet");
+        alertJ.setView(alertLayoutJ);
 
-        final AlertDialog dialog = alert.create();
-        dialog.show();
+        final AlertDialog dialogJ = alertJ.create();
+        dialogJ.show();
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -127,7 +171,7 @@ public class DashboardAcitivity extends AppCompatActivity implements View.OnClic
                 Intent intentJoin = new Intent(DashboardAcitivity.this, PlayActivity.class);
                 intentJoin.putExtra("roomID",roomID);
                 startActivity(intentJoin);
-                dialog.cancel();
+                dialogJ.cancel();
             }
         });
     }
